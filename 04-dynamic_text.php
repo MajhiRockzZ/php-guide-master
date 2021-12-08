c4226934cd07cf563a1b03ce335114b066bd62e4
Sumesh <sumesh.majhi.5@gmail.com>
2021-12-08 16:20:31 +0530

    🚀 RELEASE: The First Steps

---
+++ 04-dynamic_text.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>dynamic text</title>
+</head>
+
+<body>
+  <?php
+  $title = "Edwin Diaz Official Site";
+  ?>
+
+  <h1><?php echo $title; ?></h1>
+</body>
+
+</html>
---
+++ 05-comments.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>comments</title>
+</head>
+
+<body>
+  <?php
+  // This function below display text (one line)
+  /*
+    this is the first line (multiline)
+    this is the second line
+   */
+  echo "Hello Students";
+  ?>
+</body>
+
+</html>
