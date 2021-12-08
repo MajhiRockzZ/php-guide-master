fcfd4c3b5e163aef58de5831c08f1e5fe2c3429d
Sumesh <sumesh.majhi.5@gmail.com>
2021-12-08 19:23:30 +0530

    👌 IMPROVE: Update VS Code Config

---
+++ 06-variables.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+
+<body>
+
+<?php
+
+$name = 'Edwin';
+$number = 100;
+$NUMBER = 100;
+
+echo $name . ' ' . $number;
+
+$name = '<h1>Hello</h1>';
+
+echo $name;
+
+?>
+
+</body>
+
+</html>
