7c7277e5d450c8659b2f86138863b6c0d10c2669
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2021-12-11 21:11:29 +0530

    📦 NEW: Add PHPStorm Config

---
+++ 26-get.php
@@ -0,0 +1,22 @@
+<?php
+print_r($_GET);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+
+<body>
+<?php
+$id = 20;
+$button = 'CLICK HERE';
+?>
+<a href="26-get.php?id=<?php echo $id; ?>"><?php echo $button?></a>
+
+</body>
+
+</html>
