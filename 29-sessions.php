9b5c7d124c241168d4d0ea1e5a869b4ab0656fae
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2021-12-11 23:24:01 +0530

    📦 NEW: The Web

--- 29-sessions.php
+++ 29-sessions.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+
+$_SESSION['greeting'] = 'Hello Student';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
--- 30-session_two.php
+++ 30-session_two.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+
+echo $_SESSION['greeting'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 
