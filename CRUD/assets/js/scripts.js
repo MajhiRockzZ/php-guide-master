91071188ac0ed05f2499a25c0bd3b44a8352ce93
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2021-12-11 17:06:01 +0530

    📦 NEW: Add Sidebar

--- CRUD/assets/js/scripts.js
+++ CRUD/assets/js/scripts.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, 'user-id');
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems, 'user-id');
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, 'left');
+});
--- CRUD/includes/navbar.php
+++ CRUD/includes/navbar.php
@@ -2,6 +2,9 @@
 <nav>
   <div class="nav-wrapper">
     <a href="./" class="brand-logo">CRUD</a>
+    <a href="#" data-target="slide-out" class="sidenav-trigger">
+      <i class="material-icons">menu</i>
+    </a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
       <li><a href="create.php">Create</a></li>
       <li><a href="update.php">Update</a></li>
@@ -9,3 +12,9 @@
     </ul>
   </div>
 </nav>
+
+<ul id="slide-out" class="sidenav">
+  <li><a href="create.php">Create</a></li>
+  <li><a href="update.php">Update</a></li>
+  <li><a href="delete.php">Delete</a></li>
+</ul>
